6c540dc3b2db3f64c953256a6eba21f46c5282f9
AngelMurillo1 <murilloangel35@gmail.com>
2024-04-08 08:18:15 -0300

    use el forEach y el fetch

--- TP1/json.js
+++ TP1/json.js
@@ -1,46 +1,59 @@
-let personaje={
-    "id": 140,
-    "name": "Genital Washer",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "origin": {
-        "name": "Post-Apocalyptic Earth",
-        "url": "https://rickandmortyapi.com/api/location/8"
-    },
-    "location": {
-        "name": "Post-Apocalyptic Earth",
-        "url": "https://rickandmortyapi.com/api/location/8"
-    },
-    "image": "https://rickandmortyapi.com/api/character/avatar/140.jpeg",
-    "episode": [
-        "https://rickandmortyapi.com/api/episode/23"
-    ],
-    "url": "https://rickandmortyapi.com/api/character/140",
-    "created": "2017-12-27T18:47:44.566Z"
-}
+let personaje = {
+  id: 140,
+  name: "Genital Washer",
+  status: "Alive",
+  species: "Human",
+  type: "",
+  gender: "Male",
+  origin: {
+    name: "Post-Apocalyptic Earth",
+    url: "https://rickandmortyapi.com/api/location/8",
+  },
+  location: {
+    name: "Post-Apocalyptic Earth",
+    url: "https://rickandmortyapi.com/api/location/8",
+  },
+  image: "https://rickandmortyapi.com/api/character/avatar/140.jpeg",
+  episode: ["https://rickandmortyapi.com/api/episode/23"],
+  url: "https://rickandmortyapi.com/api/character/140",
+  created: "2017-12-27T18:47:44.566Z",
+};
 
 //console.log("El personaje " + personaje.name + " esta "+ personaje.status);
 
 //document.getElementById("result").innerHTML=("El personaje " + personaje.name + " esta " + personaje.status);
 
-function buscarPersonaje(){
 
-    const codigoInput= document.getElementById("codigo").value 
+let codigoInput = document.getElementById("codigo").value;
 
-    if(parseInt(codigoInput) === personaje.id){
+//forma para buscar los primeros 20 personajes
 
-        document.getElementById("result").innerHTML=("El personaje " + personaje.name + " esta " + personaje.status);
+function buscarPersonaje() {
 
-    }    
-    else{
-        document.getElementById("result").innerHTML=("No existe personaje con este id");
-    }
+  const codigoInput = document.getElementById("codigo").value;
 
+  document.getElementById("result").innerHTML ="";
 
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
+      console.log(data["results"]);
+
+      console.log(codigoInput);
+
+      data["results"].forEach((personaje) => {console.log(personaje.id)
+        if ((codigoInput) == personaje.id) {
+          document.getElementById("result").innerHTML =
+            "El personaje " + personaje.name + " esta " + personaje.status;
+
+        } 
+      });
+    });
 }
+
+
 
 /*
 let jugador={
