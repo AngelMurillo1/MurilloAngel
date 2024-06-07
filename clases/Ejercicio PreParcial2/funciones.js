9163fa354038286e182a1eda894ce29c5adc6e84
Angel Murillo <murilloangel35@gmail.com>
2024-06-07 11:55:41 -0300

    Ejercicio PreParcial 2

--- clases/Ejercicio PreParcial2/funciones.js
+++ clases/Ejercicio PreParcial2/funciones.js
@@ -67,3 +67,69 @@ else{
 }
 }
 
+const RandomUserAPI = `https://randomuser.me/api/`
+const RickandMortyAPI= `https://rickandmortyapi.com/api/character/3`
+
+
+class Persona{
+    constructor(nombre,foto,edad,genero,ciudad){
+        this.nombre=nombre;
+        this.foto=foto;
+        this.edad=edad;
+        this.genero=genero;
+        this.ciudad=ciudad;
+    }
+}
+
+
+function crearObjeto(){
+    let RandomUserData,RickandMortyData
+
+
+    fetch(RandomUserAPI)
+    .then(response => response.json())
+    .then(data => {
+
+
+        RandomUserData = data; // Guardar los datos de Random User
+
+
+        return fetch(RickandMortyAPI) // Hacer la solicitud a la API de Rick and Morty
+    })
+    .then(response => response.json())
+    .then(data => {
+
+
+        RickandMortyData = data; // Guardar los datos de Rick and Morty
+
+
+        // Crear un objeto con la información combinada de ambas APIs
+        let PersonajeCombinado = new Persona(RandomUserData.results[0].name.first + ' ' + RandomUserData.results[0].name.last,
+                                            RickandMortyData.image,RandomUserData.results[0].dob.age,RickandMortyData.gender,
+                                            RickandMortyData.location.name)
+
+
+       
+
+
+        console.log(PersonajeCombinado)
+
+
+        let mostrarObjeto = document.getElementById("mostrar")
+
+
+        let p = document.createElement("p")
+
+
+        p.textContent = `El personaje se llama ${PersonajeCombinado.nombre}, su edad es ${PersonajeCombinado.edad} años,
+                                    vive en ${PersonajeCombinado.ciudad} y su genero es ${PersonajeCombinado.genero}`
+       
+        mostrarObjeto.appendChild(p)
+
+
+        let image = document.createElement("img")
+        image.src = PersonajeCombinado.foto
+        mostrarObjeto.appendChild(image);
+    })
+   
+}
