1a65ebf931e204565484b2b58091a12ef91ef85f
AngelMurillo1 <164428496+AngelMurillo1@users.noreply.github.com>
2024-09-29 20:11:30 -0300

    Add files via upload

---
+++ Sistema de asistencias/Profesor/Profesor.php
@@ -0,0 +1,15 @@
+<?php
+
+class Profesor{
+
+    public function setProfesor($database, $nombre, $apellido, $dni, $email, $password, $legajo, $instituto){
+        $query= "INSERT INTO `profesores`(`id`, `nombre`, `apellido`, `dni`, `email`, `password`, `legajo`, `instituto`) 
+        VALUES (NULL,'$nombre','$apellido','$dni','$email','$password','$legajo','$instituto')";
+            $stmt = $database -> conn -> prepare($query) ; //prepara la consulta
+            $stmt -> execute(); // ejecuta la consulta
+            $usuario = $stmt -> fetchAll(); // Devuelve los datos
+    }
+
+}
+
+?>
